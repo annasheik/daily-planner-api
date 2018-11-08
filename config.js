8dee93b3e5d1ab1168ac2b7105d79bc518de86ef
Anna Kremer <sheikoanna@gmail.com>
2018-11-08 10:39:50 -0800

    Add new api key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000' || "https://daily-planner-app.herokuapp.com";
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/daily-planner-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
